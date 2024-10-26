50e6c1586030012e49ca4b801199f4bb6a7f2658
eyalp7 <eyalp717@gmail.com>
2024-10-27 00:01:13 +0300

    initial server structure

---
+++ server.py
@@ -0,0 +1,35 @@
+import socket
+
+def recieve_messsage():
+    """Recieve a message from the client"""
+
+def unpack_message_protocol():
+    """Unpack a message by the given protocol."""
+    pass
+
+def send_chat_message():
+    """Send a broadcast message."""
+    pass
+
+def promote_a_user():
+    """Give a specific user admin premissions(admin only command)"""
+    pass
+
+def kick_user():
+    """Kick a specific user(admin only command)."""
+    pass
+
+def mute_user():
+    """Mute a specific user(admin only command)."""
+    pass
+
+def private_message():
+    "Send a private message for a specific user."
+
+def start_server():
+    """Start the server and handle new clients."""
+    pass
+
+def handle_client():
+    """Handle all of the server's major tasks for each client."""
+    pass
