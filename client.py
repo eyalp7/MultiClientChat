988933052e2de754d7d18005e952d4b248e841a9
eyalp7 <eyalp717@gmail.com>
2024-10-27 00:09:13 +0300

    basic client structure

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+def get_a_username():
+    """Get the user's nickname"""
+    pass
+
+def connect_to_server():
+    """Connect to the chat server."""
+    pass
+
+def pack_a_message():
+    """Pack a message by the given protocol"""
+    pass
+
+def send_a_command():
+    """Send a specific command following the given protocol."""
+    pass
+
+
+
