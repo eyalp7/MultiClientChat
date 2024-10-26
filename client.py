111a6db8ac79ddc175d8362dbba906ed5a68e013
eyalp7 <eyalp717@gmail.com>
2024-10-27 00:26:01 +0300

    Implemented some functions

--- client.py
+++ client.py
@@ -1,11 +1,37 @@
 import socket
+import threading
+
+IP = "127.0.0.1"
+PORT = 8080
 
 def get_a_username():
-    """Get the user's nickname"""
-    pass
+    """Get a valid username from the client."""
+    username = input("Enter your username(avoid using @ at the start or using more than 16 characters): ")
+
+    #A while loop that iterates untill the username is valid
+    while len(username) > 16 or username[0] == '@':
+        if len(username) > 16:
+            print("This username is too long! ")
+        if username[0] == '@':
+            print("Try a username that doesn't start with @! ")
+        username = input("Try a new username: ")
 
 def connect_to_server():
     """Connect to the chat server."""
+    client_socket = socket.socket()
+    client_socket.connect((IP, PORT))
+
+    #Create a thread for recieving threads and sending commands to the server.
+    commands_thread = threading.Thread(target=send_a_command).start()
+    recieving_thread = threading.Thread(target=recieve_a_message).start()
+
+    #Wait for the threads to finish their task.
+    commands_thread.join()
+    recieving_thread.join()
+
+    
+def recieve_a_message():
+    """Recieve a message from the server."""
     pass
 
 def pack_a_message():
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import socket
+import threading
+
+IP = "127.0.0.1"
+PORT = 8080
 
 def recieve_messsage():
     """Recieve a message from the client"""
@@ -25,10 +29,22 @@ def mute_user():
 
 def private_message():
     "Send a private message for a specific user."
+    pass
 
 def start_server():
     """Start the server and handle new clients."""
-    pass
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+    server_socket.listen(10)
+
+    print(f"Created a server at {IP}:{PORT}")
+
+    #A while loop to create a thread for each new client.
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"New connection from: {client_address}")
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
+
 
 def handle_client():
     """Handle all of the server's major tasks for each client."""
